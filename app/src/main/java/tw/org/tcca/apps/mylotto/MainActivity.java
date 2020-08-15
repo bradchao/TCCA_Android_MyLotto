e301a5a05fc243a55e78781d951668a3caebc667
bradchao <brad@brad.tw>
2020-08-15 11:59:52 +0800

    202008151200

--- app/src/main/java/tw/org/tcca/apps/mylotto/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/mylotto/MainActivity.java
@@ -3,7 +3,10 @@ package tw.org.tcca.apps.mylotto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createLotto(View view) {
+        HashSet<Integer> set = new HashSet<>();
+        while (set.size()<6){
+            set.add((int)(Math.random()*49+1));
+        }
+        Log.v("bradlog", set.toString());
     }
 }
