cd1f729f014bd3d8b1bd9aeae70c28a0224deb78
bradchao <brad@brad.tw>
2020-08-15 13:07:38 +0800

    202008151307

--- app/src/main/java/tw/org/tcca/apps/mylotto/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/mylotto/MainActivity.java
@@ -5,15 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mesg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mesg = findViewById(R.id.lotto);
     }
 
     public void createLotto(View view) {
@@ -22,5 +25,9 @@ public class MainActivity extends AppCompatActivity {
             set.add((int)(Math.random()*49+1));
         }
         Log.v("bradlog", set.toString());
+
+        mesg.setText(set.toString());
+
+
     }
 }
